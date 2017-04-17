50e2d1efa21e7bdffa4e94d4fa50682a85553b41
Marco Aurelio da Costa <costa@gamic.com>
2017-04-17 09:54:41 -0300

    Added object Polyline

--- graphics.py
+++ graphics.py
@@ -60,7 +60,10 @@ Programming: An Introduction to Computer Science" by John Zelle,
 published by Franklin, Beedle & Associates.  Also see
 http://mcsp.wartburg.edu/zelle/python for a quick reference"""
 
-__version__ = "5.0"
+__version__ = "5.1"
+
+# Version 5.1 04/17/2017
+#     * added Polyline object
 
 # Version 5 8/26/2016
 #     * update at bottom to fix MacOS issue causing askopenfile() to hang
@@ -720,6 +723,45 @@ class Polygon(GraphicsObject):
             args.append(y)
         args.append(options)
         return GraphWin.create_polygon(*args)
+
+
+class Polyline(GraphicsObject):
+
+    def __init__(self, *points):
+        # if points passed as a list, extract it
+        if len(points) == 1 and type(points[0]) == type([]):
+            points = points[0]
+        self.points = list(map(Point.clone, points))
+        GraphicsObject.__init__(self, ["outline","width"])
+
+    def __repr__(self):
+        return "Polyline"+str(tuple(p for p in self.points))
+
+    def clone(self):
+        other = Polyline(*self.points)
+        other.config = self.config.copy()
+        return other
+
+    def getPoints(self):
+        return list(map(Point.clone, self.points))
+
+    def _move(self, dx, dy):
+        for p in self.points:
+            p.move(dx,dy)
+
+    def _draw(self, canvas, options):
+        args = [canvas]
+        for p in self.points:
+            x,y = canvas.toScreen(p.x,p.y)
+            args.append(x)
+            args.append(y)
+        options = options.copy()
+        if "outline" in options:
+          options["fill"] = options["outline"]
+          del options["outline"]
+        args.append(options)
+        return GraphWin.create_line(*args)
+
 
 class Text(GraphicsObject):
 
